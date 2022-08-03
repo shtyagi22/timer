fee1811a2b748ea56115caafe4c99eb796280249
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-03 16:37:33 -0400

    refactoring the code

--- timer1.js
+++ timer1.js
@@ -1,6 +1,6 @@
 var args = process.argv.slice(2);
-//args.sort(function(a, b){return a-b});
-
+args.sort(function (a, b) { return a - b });
+let time = 0;
 for (let arg of args) {
   time = 1000 * arg
   setTimeout(() => {
